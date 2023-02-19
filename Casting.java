fb233fe49b7133789a1c95bdde2544278c6bf2f3
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-02-19 20:42:32 +0530

    Type Casting

---
+++ Casting.java
@@ -0,0 +1,10 @@
+public class Casting {
+    public static void main(String[] args) {
+        double d = 4.5;
+
+        int i = (int) d;
+
+        System.out.println(d);
+        System.out.println(i);
+    }
+}
